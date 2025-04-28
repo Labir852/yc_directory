06641b23bc66bca74f2c349afd6736cbbe099a18
Tanvir Anjum Labir <tanvir.labir@gmail.com>
2025-04-28 17:00:44 +0600

    latest

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,9 +3,10 @@ import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const StartupCard = ({post}:{post : StartupCardType}) => {
-    const {_createdAt, views,author:{_id:authorId,name},title,category,_id,image} = post;
+    const {_createdAt, views,author:{_id:authorId,name},title,category,_id,image,description} = post;
   return (
     <li className='startup-card group'>
         <div className='flex-between'>
@@ -39,6 +40,20 @@ const StartupCard = ({post}:{post : StartupCardType}) => {
                     className="rounded-full"/>
             </Link>
         </div>
+        <Link href={`/startup/${_id}`}>
+                <p className='startup-card-desc'>{description}</p>
+                <img src={image} alt='placeholder' className='startup-card_img'/>
+            </Link>
+            <div className="flex-between gap-3 mt-5">
+            <Link href={`/?query=${category.toLowerCase()}`}>
+                <p className='text-16-medium'>{category}</p>
+                </Link>
+                <Button className='startup-card_btn' asChild>
+                    <Link href={`/startup/${_id}`}>
+                    Details
+                    </Link>
+                </Button>
+            </div>
     </li>
   )
 }
